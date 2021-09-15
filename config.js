81503cfd7988f964814c2c6330ce148c1296cf0b
Mike W <mike@commonobject.com>
2021-09-14 22:43:46 -0700

    update install script

--- config.js
+++ config.js
@@ -58,7 +58,7 @@ module.exports = {
     '1password',
     'alfred',
     'atext',
-    'cleanmymac',
+    //'cleanmymac',
     'docker',
     //'dropbox',
     //'evernote',
@@ -91,5 +91,17 @@ module.exports = {
     'prettyjson',
     'trash',
     'vtop'
-  ]
+  ],
+  mas: [
+    //com.apple.dt.Xcode (10.2.1)
+    '497799835',
+    //com.if.Amphetamine (4.1.6)
+    //'937984704',
+    //net.shinyfrog.bear (1.6.15)
+    //'1091189122',
+    //com.monosnap.monosnap (3.5.8)
+    //'540348655',
+    //com.app77.pwsafemac (4.17)
+    //'520993579',
+  ],
 };
